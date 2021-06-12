cb808ad8519802b7727c20117dfe9d380f104ba8
tuttibalutti <emmajbartl@gmail.com>
2021-06-12 23:49:18 +0200

    updated reducer and server.js. Created schemas for user and baths

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import bodyParser from 'body-parser'
+// import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
+import listEndpoints from 'express-list-endpoints'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/BadenBaden"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, })
 mongoose.Promise = Promise
 
 
@@ -14,6 +17,25 @@ mongoose.Promise = Promise
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
+
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true, 
+		minlength: 5,
+		maxlength: 15,
+		unique: true,
+		trim: true
+	},
+	password: {
+		type: String, 
+		required: true,
+	},
+	accessToken: {
+		type: String, 
+		default: () => crypto.randomBytes(128).toString('hex')
+	}
+})
 
 const bathSchema = new mongoose.Schema({
 	name: String,//add validation?	
@@ -31,15 +53,56 @@ const bathSchema = new mongoose.Schema({
 	}
 })
 
+const User = mongoose.model('User', userSchema)
+
 const Bath = mongoose.model('Bath', bathSchema)
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header('Authorization')
+  try {
+    const user = await User.findOne({ accessToken })
+    if (user) {
+      next()
+    } else {
+      res.status(401).json({ success: false, message: 'Not authorized' })
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, message: 'Invalid request', error })
+  }
+}
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app))
+})
+
+app.post('/users', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const salt = bcrypt.genSaltSync()
+    const newUser = new User({
+      username,
+      password: bcrypt.hashSync(password, salt)
+    })
+		await newUser.save()
+    res.json({
+      success: true,
+      userId: newUser._id,
+      username: newUser.username,
+      accessToken: newUser.accessToken,
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Could not create user',
+      error
+    })
+  }
 })
 
 app.post('/bath', async (req, res) => {
--- frontend/src/reducers/bath.js
+++ frontend/src/reducers/bath.js
@@ -1,10 +1,37 @@
-// import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
-// //need to implement local storage??
+//need to implement local storage??
 
-// const initialItems = {
-//     userName: "",
+const initialState = {
+	userId: null,
+	username: null,
+	accessToken: null,
+	currentPosition: null,
+	baths: [],
+	errors: null
+}
 
-//     currentPosition: null,
-
-// // }
+const bath = createSlice({
+	name: "bath",
+	initialState,
+	reducers: {
+		setUserId: (store, action) => {
+			store.userId = action.payload
+		},
+		setUsername: (store, action) => {
+			store.username = action.payload
+		},
+		setAccessToken: (store, action) => {
+			store.accessToken = action.payload
+		},
+		setCurrentPosition: (store, action) => {
+			store.currentPosition = action.payload
+		},
+		setBaths: (store, action) => {
+			store.baths = action.payload
+		},
+		setErrors: (store, action) => {
+			store.errors = action.payload
+		}
+	}
+})
